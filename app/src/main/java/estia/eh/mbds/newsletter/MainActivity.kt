cd749eb91c6727e2a39faf010d8b52c0b911d691
n-tamanini <n.tamanini@net.estia.fr>
2020-11-05 15:03:08 +0100

    first commit

---
+++ app/src/main/java/estia/eh/mbds/newsletter/MainActivity.kt
@@ -0,0 +1,11 @@
+package estia.eh.mbds.newsletter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
