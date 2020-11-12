7ebb0e4459fdc97d155d2f1dbad64429b762f324
Alphonse Fournier <a.fournier@net.estia.fr>
2020-11-12 10:39:27 +0100

    adapter

--- app/src/main/java/estia/eh/mbds/newsletter/fragment/ListArticlesFragment.kt
+++ app/src/main/java/estia/eh/mbds/newsletter/fragment/ListArticlesFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -12,7 +13,10 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import estia.eh.mbds.newsletter.R
 import estia.eh.mbds.newsletter.adapter.ListArticlesAdapter
 import estia.eh.mbds.newsletter.data.ArticleRepository
+import estia.eh.mbds.newsletter.models.Article
 import estia.eh.mbds.newsletter.models.NavigationListener
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ListArticlesFragment : Fragment()  {
     private lateinit var recyclerView: RecyclerView
@@ -46,9 +50,33 @@ class ListArticlesFragment : Fragment()  {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val neighbors = ArticleRepository.getInstance().getArticles()
-        val adapter = ListArticlesAdapter(neighbors)
+        val articles = ArticleRepository.getInstance().getArticles()
+        val adapter = ListArticlesAdapter(articles)
         recyclerView.adapter = adapter
+    }
+
+
+
+    /** ----------------------------------------------------------------
+
+     * Récupère la liste des articles dans un thread secondaire
+     */
+    private fun getArticles() {
+        lifecycleScope.launch(Dispatchers.IO) {
+            val articles = ArticleRepository.getInstance().getArticles()
+        }
+    }
+
+    /**
+     * Rempli le recyclerview avec les données récupérées dans le web service
+     * Cette action doit s'effectuer sur le thread principale
+     * Car on ne peut mas modifier les éléments de vue dans un thread secondaire
+     */
+    private fun bindData(articles: List<Article>) {
+        lifecycleScope.launch(Dispatchers.Main) {
+            //créer l'adapter
+            //associer l'adapteur au recyclerview
+        }
     }
 
 
