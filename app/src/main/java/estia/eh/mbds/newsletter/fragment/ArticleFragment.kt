37f2b0264e3c596c21bdce92902d8ce0436c2549
Lisa <l.momas@net.estia.fr>
2020-11-13 11:56:02 +0100

    travail click articleV2

--- app/src/main/java/estia/eh/mbds/newsletter/fragment/ArticleFragment.kt
+++ app/src/main/java/estia/eh/mbds/newsletter/fragment/ArticleFragment.kt
@@ -1,12 +1,16 @@
 package estia.eh.mbds.newsletter.fragment
 
+import android.content.Intent
+import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import estia.eh.mbds.newsletter.NavigationListener
 import estia.eh.mbds.newsletter.R
+
 
 class ArticleFragment : Fragment() {
 
@@ -20,6 +24,11 @@ class ArticleFragment : Fragment() {
         (activity as? NavigationListener)?.let {
             it.updateTitle(R.string.article_name)
         }
+
+        val editWeb: WebView = findViewById(R.id.url)
+        val intent = Intent.extra.getString(context)
+        val url = intent.getStringExtra("url")
+        editWeb.loadUrl(url!!)
 
         return view
     }
