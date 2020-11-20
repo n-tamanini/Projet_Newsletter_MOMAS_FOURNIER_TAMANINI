5cdc72fb2bf834bb5d6aac654936dfb5bb308b81
Lisa <l.momas@net.estia.fr>
2020-11-20 15:05:14 +0100

    debut spinners

--- app/src/main/java/estia/eh/mbds/newsletter/MainActivity.kt
+++ app/src/main/java/estia/eh/mbds/newsletter/MainActivity.kt
@@ -1,21 +1,27 @@
 package estia.eh.mbds.newsletter
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import estia.eh.mbds.newsletter.fragment.ArticleFragment
+import androidx.fragment.app.Fragment
 import estia.eh.mbds.newsletter.fragment.AboutUsFragment
 import estia.eh.mbds.newsletter.fragment.ListArticlesFragment
 import estia.eh.mbds.newsletter.fragment.ListFavoritesFragment
 import estia.eh.mbds.newsletter.fragment.PageAccueilFragment
+import estia.eh.mbds.newsletter.models.Constants
+
 
 private lateinit var toolbar: Toolbar
-private lateinit var toolbarOpt: Toolbar
 private lateinit var clickButton: Button
 private lateinit var spinnerCategory: Spinner
 private lateinit var spinnerCountry: Spinner
@@ -27,9 +33,6 @@ class MainActivity : AppCompatActivity(), NavigationListener {
         setContentView(R.layout.activity_main)
         toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
-        clickButton = findViewById(R.id.btn_click_me)
-        spinnerCategory = findViewById(R.id.spinner_category)
-        spinnerCountry = findViewById(R.id.spinner_country)
 
 
         changeFragment(PageAccueilFragment())
@@ -78,6 +81,103 @@ class MainActivity : AppCompatActivity(), NavigationListener {
         }
         return super.onOptionsItemSelected(item)
     }
+    private var selectedCountryPos:Int =0
+    private var selectedCategoriePos:Int = 0
+    private var selectedCountry:String = "All"
+fun clickBtn() {
+    val view = LayoutInflater.from(this).inflate(R.layout.page_accueil_fragment, null)
+    clickButton = findViewById(R.id.btn_click_me)
+    spinnerCategory = findViewById(R.id.spinner_category)
+    spinnerCountry = findViewById(R.id.spinner_country)
+
+    val COUNTRIES = arrayOf<String>(
+        "All",
+        "ae",
+        "ar",
+        "at",
+        "au",
+        "be",
+        "bg",
+        "br",
+        "ca",
+        "ch",
+        "cn",
+        "co",
+        "cu",
+        "cz",
+        "de",
+        "eg",
+        "fr",
+        "gb",
+        "gr",
+        "hk",
+        "hu",
+        "id",
+        "ie",
+        "il",
+        "in",
+        "it",
+        "jp",
+        "kr",
+        "lt",
+        "lv",
+        "ma",
+        "mx",
+        "my",
+        "ng",
+        "nl",
+        "no",
+        "nz",
+        "ph",
+        "pl",
+        "pt",
+        "ro",
+        "rs",
+        "ru",
+        "sa",
+        "se",
+        "sg",
+        "si",
+        "sk",
+        "th",
+        "tr",
+        "tw",
+        "ua",
+        "us",
+        "ve",
+        "za"
+    )
+
+    val adapterCountry =
+        ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, COUNTRIES)
+    adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+    spinnerCountry.adapter = adapterCountry
+
+    spinnerCountry.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+          selectedCountry =COUNTRIES[position]
+            selectedCountryPos = position
+            /*  val selectedItem = parent.getItemAtPosition(position).toString()
+            if (selectedItem == "Add new category") {
+                // do your stuff*/
+            }
+        } // to close the onItemSelected
+
+        override fun onNothingSelected(parent: AdapterView<*>) {
+
+        }
+    }
+
+    clickButton.setOnClickListener(object : View.OnClickListener{
+        override fun onClick(v: View?) {
+            loadSources()
+        }
 
 
+       /* val intent = Intent(this, ListArticlesFragment::class.java).apply {
+        }
+        startActivity(intent)*/
+    }
+
+}
 }
